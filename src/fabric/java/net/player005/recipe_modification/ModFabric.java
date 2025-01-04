95fc2164faae53095ccb21edd2615707da626c0e
player005 <me@player005.net>
2025-01-04 15:06:07 +0100

    add initial mod metadata

---
+++ src/fabric/java/net/player005/recipe_modification/ModFabric.java
@@ -0,0 +1,10 @@
+package net.player005.recipe_modification;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ModFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/neoforge/java/net/player005/recipe_modification/ModNeoforge.java
@@ -0,0 +1,7 @@
+package net.player005.recipe_modification;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(RecipeModification.modID)
+public class ModNeoforge {
+}
