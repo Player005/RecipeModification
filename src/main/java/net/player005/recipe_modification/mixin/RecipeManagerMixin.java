f7d8eac29918be4d9778a231bd699fd6b97e6d12
player005 <me@player005.net>
2025-01-12 21:34:20 +0100

    temporarily change entrypoint for recipe manager getter (neoforge only)

--- src/main/java/net/player005/recipe_modification/mixin/RecipeManagerMixin.java
+++ src/main/java/net/player005/recipe_modification/mixin/RecipeManagerMixin.java
@@ -1,7 +1,6 @@
 package net.player005.recipe_modification.mixin;
 
 import net.minecraft.world.item.crafting.RecipeManager;
-import net.player005.recipe_modification.RecipeModification;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,6 +10,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class RecipeManagerMixin {
     @Inject(method = "apply(Ljava/util/Map;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V", at = @At("RETURN"))
     void onRecipeManagerLoad(CallbackInfo ci) {
-        RecipeModification.onRecipeManagerLoad((RecipeManager) (Object) this);
+        //RecipeModification.onRecipeManagerLoad((RecipeManager) (Object) this);
     }
 }
--- src/neoforge/java/net/player005/recipe_modification/ModNeoforge.java
+++ src/neoforge/java/net/player005/recipe_modification/ModNeoforge.java
@@ -4,6 +4,7 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.AddReloadListenerEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
 
 @Mod(RecipeModification.modID)
 public class ModNeoforge {
@@ -12,5 +13,8 @@ public class ModNeoforge {
 
     public ModNeoforge(IEventBus modEventBus) {
         NeoForge.EVENT_BUS.addListener(AddReloadListenerEvent.class, event -> event.addListener(new RecipeModifierManager()));
+        NeoForge.EVENT_BUS.<ServerStartingEvent>addListener(event -> {
+            RecipeModification.onRecipeManagerLoad(event.getServer().getRecipeManager());
+        });
     }
 }
