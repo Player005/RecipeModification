a69ea97ddfba03a884d886b264619f09c50c9afb
player005 <me@player005.net>
2025-01-17 18:40:44 +0100

    fix unit tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -150,7 +150,14 @@ val testmodModImplementation by configurations.getting
 val testmodCompileOnly by configurations.getting
 val testModImplementation by configurations.getting
 
+val impls by configurations.creating
 val global by configurations.creating
+
+sourceSets.forEach {
+    if (!it.name.startsWith("1.")) return@forEach
+    it.runtimeClasspath += impls
+    it.compileClasspath += impls
+}
 
 sourceSets.forEach {
     it.runtimeClasspath += global
@@ -160,8 +167,8 @@ sourceSets.forEach {
 dependencies {
     global("org.jetbrains:annotations:26.0.1")
 
-    global("net.fabricmc:sponge-mixin:0.15.5+mixin.0.8.7")
-    global(annotationProcessor("io.github.llamalad7:mixinextras-common:0.4.1")!!)
+    impls("net.fabricmc:sponge-mixin:0.15.5+mixin.0.8.7")
+    impls(annotationProcessor("io.github.llamalad7:mixinextras-common:0.4.1")!!)
 
     testmodCompileOnly(sourceSets.main.get().output)
     testmodModImplementation(tasks.getByName("remapNeoforgeJar").outputs.files)
